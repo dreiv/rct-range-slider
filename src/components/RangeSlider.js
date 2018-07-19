ebc0f74981f7ae2a3fa0853459ea61c22cf88d21
dreiv <dreivoicu@gmail.com>
2018-07-19 23:50:01 +0300

    added state to slider

--- src/components/RangeSlider.js
+++ src/components/RangeSlider.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './RangeSlider.css'
 
-export default function RangeSlider({ className, ...props }) {
-	return (
-		<label className={`toggle ${className}`}>
-			<input type="range" defaultValue="2" min="0" max="4" {...props} />
-		</label>
-	)
+export default class RangeSlider extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			progress: 2,
+		}
+	}
+
+	handleChange = ({ target: { value } }) => {
+		this.setState({ progress: +value })
+	}
+
+	render() {
+		const { className, ...props } = this.props
+		const { progress } = this.state
+		return (
+			<label className={`toggle ${className}`}>
+				<p>hello try this</p>
+				<input
+					type="range"
+					value={progress}
+					min="0"
+					max="4"
+					onChange={this.handleChange}
+					{...props}
+				/>
+
+				<p>progress: {progress}</p>
+			</label>
+		)
+	}
 }
